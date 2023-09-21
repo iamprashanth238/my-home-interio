bcc1e37ec34db50c44fe49bd18c9dfe06989ee5a
Kattoju Prashanth <kattojuprashanth238@gmail.com>
2023-09-21 16:32:49 +0530

    New Design page
    Added Design page it contain different design where users can select different design

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -3,6 +3,11 @@ const homepage = (req, res) => {
     res.render('index', {title:'My-Home-Interio'});
 };
 
+const designpage = (req, res) => {
+    res.render('design',{title:"Design Interio"});
+};
+
 module.exports = {
-    homepage
+    homepage,
+    designpage
 }
